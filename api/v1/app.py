efe6f3cf3883d3148fbc018c405150c5c6cc77aa
Gogo-Eng <progressgogochinda@gamil.com>
2024-06-05 01:51:26 +0100

    killer

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,37 +3,18 @@
 """
 
 
+from os import getenv
 from flask import Flask
-from api.v1.views import app_views
-import os
 from flask import jsonify
 from models import storage
-from flask_cors import CORS
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Close the storage connection"""
-    storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """
-    Handling the 404 json
-    Not Found errors
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-if __name__ == "__main__":
-    """Run the Flask application"""
-    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', default='5000')
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
